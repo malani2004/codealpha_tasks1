1473db79f3ff351e31af508d4bb9af035981b069
malani2004 <malani2004mal@gmail.com>
2025-01-26 19:11:09 +0530

    first commit

---
+++ apps/settings.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+#For Django versions >= 3.0, you can use Path (recommended)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# corein/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'users',
+    'posts',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
